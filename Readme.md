3e44b4dac3b88729aa60bc626ef89aea89fb785f
prashanth-sams <sams.prashanth@gmail.com>
2021-06-29 23:55:17 +0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#### Wiremock JVM Boilerplate
+> Backend with WireMock stubbed data
+
+```shell
+mvn clean test
+```
--- src/test/java/org/wmock/WireMockApiTests.java
+++ src/test/java/org/wmock/WireMockApiTests.java
@@ -1,6 +1,9 @@
 package org.wmock;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import static io.restassured.RestAssured.*;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,27 +15,38 @@ public class WireMockApiTests
 
     @BeforeEach
     void initWireMock(){
-        wireMockServer = new WireMockServer(7777);
+        wireMockServer = new WireMockServer(8080);
         wireMockServer.start();
         initStub();
     }
 
     private void initStub() {
-        configureFor("localhost", 7777);
+        configureFor("localhost", 8080);
 
         stubFor(get(urlEqualTo("/users/all"))
                 .inScenario("getAllUsers")
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withStatus(200)
-                        .withBody("")
+                        .withBody("{ \"id\": 1, \"name\": \"Prashanth Sams\", \"gender\": \"male\", \"age\": 31 }, { \"id\": 2, \"name\": \"John Smith\", \"gender\": \"male\", \"age\": 40 }")
                 )
         );
     }
 
     @Test
-    public void firstMethod()
+    public void getAllUsersTest()
     {
-        System.out.println("am here");
+        given()
+                .when()
+                    .get("http://localhost:8080/users/all")
+                .then()
+                    .assertThat()
+                    .statusCode(200);
+    }
+
+    @AfterEach
+    void resetMock(){
+        wireMockServer.resetAll();
+        wireMockServer.stop();
     }
 }
